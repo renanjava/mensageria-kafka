19c21bf6e31a9b8989cbd79f01283d2dbfa85dd1
Renan <renancvr777@gmail.com>
2024-11-14 00:48:42 -0300

    feat: :sparkles:

---
+++ kafka/src/main/java/com/kafka/mensageria/SimpleServer.java
@@ -0,0 +1,37 @@
+package com.kafka.mensageria;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+
+public class SimpleServer {
+    public static void main(String[] args) throws IOException {
+        int port = 3001;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", new RootHandler());
+
+        server.setExecutor(null);
+
+        System.out.println("Servidor rodando na porta " + port);
+        
+        server.start();
+    }
+
+    static class RootHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Olá, servidor em Java está rodando!";
+            
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
+
